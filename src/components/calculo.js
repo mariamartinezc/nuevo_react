a7d72d17a8e80c4689c529cd72a36448081d9026
pauloberrios44 <166758722+pauloberrios44@users.noreply.github.com>
2025-10-10 20:23:44 -0300

    Avances programacion, se finaliza el componente de calculo de remuneraciones

--- src/components/calculo.js
+++ src/components/calculo.js
@@ -6,98 +6,138 @@ function CalculoRemuneracion() {
 
     const [sueldoBruto, setSueldoBruto] = useState(0);
     const [gratificacionLegal, setGratificacionLegal] = useState(0);
-
-    
+    const [tipoPrevision, setTipoPrevision] = useState("");
+    const [montoMovilizacion, setMontoMovilizacion] = useState(0);
+    const [montoColacion, setMontoColacion] = useState(0);
+    const [montoViatico, setMontoViatico] = useState(0);
 
     // constantes
     const porcentajeComisionAFP = 0.009;
-    const porcentajeComisionSalud = 0.07;
+    const porcentajeComisionSalud = tipoPrevision == 1 ? 0.07 : 0.1;
     const porcentajeImpuestoUnico = 0.01;
     const porcentajeSeguroCesantia = 0.006;
 
+    // resultado
+    const resultadoSueldoImponible = parseInt(sueldoBruto) + parseInt(gratificacionLegal);
+    const resultadoComisionAFP = parseInt(resultadoSueldoImponible) * porcentajeComisionAFP;
+    const resultadoAFP = parseInt(resultadoSueldoImponible) * 0.1;
+    const resultadoSalud = parseInt(resultadoSueldoImponible * porcentajeComisionSalud);
+    const resultadoImpuestoUnico = parseInt(resultadoSueldoImponible * porcentajeImpuestoUnico);
+    const resultadoSeguroCesantia = parseInt(resultadoSueldoImponible * porcentajeSeguroCesantia);
+    const resultadoMontoMovilizacion = parseInt(montoMovilizacion);
+    const resultadoMontoColacion = parseInt(montoColacion);
+    const resultadoMontoViatico = parseInt(montoViatico);
 
-    const resultadoComisionAFP = parseInt(sueldoBruto) * porcentajeComisionAFP;
-    const resultadoAFP = parseInt(sueldoBruto) * 0.1;
-    const resultadoSalud = parseInt(sueldoBruto * porcentajeComisionSalud);
-    const resultadoImpuestoUnico = parseInt(sueldoBruto * porcentajeImpuestoUnico);
-    const resultadoSeguroCesantia = parseInt(sueldoBruto * porcentajeSeguroCesantia);
+    const liquidoAPagar = resultadoSueldoImponible + resultadoMontoColacion + resultadoMontoMovilizacion + resultadoMontoViatico - resultadoComisionAFP - resultadoAFP - resultadoSalud - resultadoImpuestoUnico - resultadoSeguroCesantia;
 
-    const liquidoAPagar = sueldoBruto - resultadoComisionAFP - resultadoAFP - resultadoSalud - resultadoImpuestoUnico - resultadoSeguroCesantia;
-    
 
     console.log('sueldoBruto: ' + sueldoBruto);
     console.log('gratificacionLegal: ' + gratificacionLegal);
     console.log(porcentajeImpuestoUnico);
     console.log(porcentajeSeguroCesantia);
-
     console.log(parseFloat(porcentajeComisionAFP));
+    console.log('Tipo prevision: ' + tipoPrevision);
 
 
     return (
-        <div className='row mt-3'>
-            <div className='col-lg-6'>
-                <h4>Datos sueldo</h4>
-                <label className='form-label' htmlFor='sueldoBruto'>Ingrese sueldo bruto</label>
-                <input id='sueldoBruto' name='sueldoBruto' placeholder='1000000' type='number' className='form-control' value={sueldoBruto} onChange={(e) => setSueldoBruto(e.target.value)}></input>
+        <div>
+            <div className='row'>
+                <div className='col-lg-12 text-center bg-secondary text-white p-3 rounded'>
+                    <h2>Demo de cálculo</h2>
+                    <h4>Sueldo líquido</h4>
+                </div>
+            </div>
 
-                <div className='form-group mt-3'>
-                    <label className='form-label' htmlFor='gratificacionLegal'>Gratificación Legal</label>
-                    <input type='number' id='gratificacionLegal' name='gratificacionLegal' className='form-control' value={gratificacionLegal} placeholder='250000' onChange={(e) => setGratificacionLegal(e.target.value)}></input>
+            <div className='row mt-3'>
+                <div className='col-lg-6'>
+                    <h4>Datos sueldo</h4>
+                    <label className='form-label' htmlFor='sueldoBruto'>Ingrese sueldo bruto</label>
+                    <input id='sueldoBruto' name='sueldoBruto' placeholder='1000000' type='number' className='form-control' value={sueldoBruto} onChange={(e) => setSueldoBruto(e.target.value)}></input>
+
+                    <div className='form-group mt-3'>
+                        <label className='form-label' htmlFor='gratificacionLegal'>Gratificación Legal</label>
+                        <input type='number' id='gratificacionLegal' name='gratificacionLegal' className='form-control' value={gratificacionLegal} placeholder='250000' onChange={(e) => setGratificacionLegal(e.target.value)}></input>
+                    </div>
+
+                    <div className='form-group mt-3'>
+                        <label className='form-label' htmlFor='tipoPrevision'>Seleccione previsión de salud</label>
+                        <select className='form-select' id='tipoPrevision' name='tipoPrevision' value={tipoPrevision} onChange={(e) => setTipoPrevision(e.target.value)}>
+                            <option>Seleccione prevision</option>
+                            <option value={1}>Fonasa</option>
+                            <option value={2}>Isapre</option>
+                        </select>
+                    </div>
+
+                    <h4 className='mt-4'>No imponibles</h4>
+                    <div className='form-group mt-3'>
+                        <label className='form-label' htmlFor='montoMovilizacion'>Monto movilización</label>
+                        <input type='number' id='montoMovilizacion' name='montoMovilizacion' className='form-control' value={montoMovilizacion} placeholder='20000' onChange={(e) => setMontoMovilizacion(e.target.value)}></input>
+                    </div>
+
+                    <div className='form-group mt-3'>
+                        <label className='form-label' htmlFor='montoColacion'>Monto colación</label>
+                        <input type='number' id='montoColacion' name='montoColacion' className='form-control' value={montoColacion} placeholder='20000' onChange={(e) => setMontoColacion(e.target.value)}></input>
+                    </div>
+
+                    <div className='form-group mt-3'>
+                        <label className='form-label' htmlFor='montoViatico'>Monto viático</label>
+                        <input type='number' id='montoViatico' name='montoViatico' className='form-control' value={montoViatico} placeholder='20000' onChange={(e) => setMontoViatico(e.target.value)}></input>
+                    </div>
+
                 </div>
 
+                <div className='col-lg-6'>
+                    <Table striped bordered hover>
+                        <thead>
+                            <tr>
+                                <th>#</th>
+                                <th>Concepto</th>
+                                <th>Monto</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>1</td>
+                                <td>Sueldo Bruto</td>
+                                <td>$ {parseInt(sueldoBruto).toLocaleString()}</td>
+                            </tr>
+                            <tr>
+                                <td>2</td>
+                                <td>(-) AFP (10%)</td>
+                                <td>$ {resultadoAFP.toLocaleString()}</td>
+                            </tr>
+                            <tr>
+                                <td>3</td>
+                                <td>(-) Comision AFP ({(porcentajeComisionAFP * 100).toFixed(1)} %) </td>
+                                <td>$ {resultadoComisionAFP.toLocaleString()}</td>
+                            </tr>
+                            <tr>
+                                <td>4</td>
+                                <td>(-) Salud ({(porcentajeComisionSalud * 100).toFixed(1)} %)</td>
+                                <td>$ {resultadoSalud.toLocaleString()}</td>
+                            </tr>
+                            <tr>
+                                <td>5</td>
+                                <td>(-) Impuesto único ({(porcentajeImpuestoUnico * 100).toFixed(1)} %)</td>
+                                <td>$ {resultadoImpuestoUnico.toLocaleString()}</td>
+                            </tr>
+                            <tr>
+                                <td>6</td>
+                                <td>(-) Seguro cesantía ({(porcentajeSeguroCesantia * 100).toFixed(1)} %) </td>
+                                <td>$ {resultadoSeguroCesantia.toLocaleString()}</td>
+                            </tr>
+
+                            <tr style={{ fontSize: '18px' }}>
+                                <td colSpan={2}>Total</td>
+                                <td>$ {liquidoAPagar.toLocaleString()}</td>
+                            </tr>
+                        </tbody>
+                    </Table>
+                </div>
 
             </div>
-
-            <div className='col-lg-6'>
-                <Table striped bordered hover>
-                    <thead>
-                        <tr>
-                            <th>#</th>
-                            <th>Concepto</th>
-                            <th>Monto</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>1</td>
-                            <td>Sueldo Bruto</td>
-                            <td>{parseInt(sueldoBruto).toLocaleString()}</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                            <td>(-) AFP (10%)</td>
-                            <td>{resultadoAFP.toLocaleString()}</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                            <td>Comision AFP</td>
-                            <td>{resultadoComisionAFP.toLocaleString()}</td>
-                        </tr>
-                        <tr>
-                            <td>4</td>
-                            <td>Salud</td>
-                            <td>{resultadoSalud.toLocaleString()}</td>
-                        </tr>
-                        <tr>
-                            <td>5</td>
-                            <td>Impuesto único</td>
-                            <td>{resultadoImpuestoUnico.toLocaleString()}</td>
-                        </tr>
-                        <tr>
-                            <td>6</td>
-                            <td>Seguro cesantía</td>
-                            <td>{resultadoSeguroCesantia.toLocaleString()}</td>
-                        </tr>
-
-                        <tr>
-                            <td colSpan={2}>Total</td>
-                            <td>{liquidoAPagar.toLocaleString()}</td>
-                        </tr>
-                    </tbody>
-                </Table>
-            </div>
-
         </div>
+
     );
 }
 
